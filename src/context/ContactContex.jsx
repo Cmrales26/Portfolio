86a45a87121540eefe5963bd1b63e4c9e356d7c7
Cmrales26 <camilomorales2615@gmail.com>
2024-08-28 20:44:19 -0500

    Getting pdf from backend

--- src/context/ContactContex.jsx
+++ src/context/ContactContex.jsx
@@ -1,28 +1,28 @@
-import { createContext, useContext } from "react";
-import PropTypes from "prop-types";
+import { createContext, useContext } from 'react';
+import PropTypes from 'prop-types';
 
 export const ContactContex = createContext();
 
 export const useContact = () => {
   const context = useContext(ContactContex);
-  if (!context) throw new Error("There is no Icon Context");
+  if (!context) throw new Error('There is no Icon Context');
   return context;
 };
-const SERVER_URL =
-  import.meta.env.VITE_SERVER_URL || "http://localhost:5000/api";
+export const SERVER_URL =
+  import.meta.env.VITE_SERVER_URL || 'http://localhost:5000/api';
 
 export const ContactProvider = ({ children }) => {
-  const SendEmail = async (data) => {
+  const SendEmail = async data => {
     const suit = await fetch(`${SERVER_URL}/sendemail`, {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: data,
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: data
     });
     return suit;
   };
 
   ContactProvider.propTypes = {
-    children: PropTypes.object.isRequired,
+    children: PropTypes.object.isRequired
   };
 
   return (
